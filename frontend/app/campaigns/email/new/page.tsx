7456657769bb800f9fd090e1affd50e1018bc666
rishibalanagu0108 <rishibalanagu0108@gmail.com>
2026-06-16 19:42:04 +0530

    Test the CI/CD functionality

--- frontend/app/campaigns/email/new/page.tsx
+++ frontend/app/campaigns/email/new/page.tsx
@@ -1,6 +1,7 @@
 import { PageWrapper } from "@/components/layout/PageWrapper"
 import { EmailCampaignWizard } from "@/components/campaigns/EmailCampaignWizard"
 
+
 export default function NewEmailCampaignPage() {
   return (
     <PageWrapper>
